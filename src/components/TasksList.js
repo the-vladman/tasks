3bfce94b04266f7a4f2e2ccbd45e2c772d51dc91
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2019-03-15 11:06:39 -0600

    (component) TasksList

---
+++ src/components/TasksList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { actionCreators } from '../actions'
+import {  Row, Col, List, } from 'antd';
+import AddTask from './AddTask'
+import TaskItem from './TaskItem'
+
+class TasksList extends Component {
+  render() {
+    const { tasks } = this.props
+    const { dispatch } = this.props
+    return (
+      <List
+        size="large"
+        header={<AddTask />}
+        footer={<div>Footer</div>}
+        bordered
+        dataSource={tasks}
+        renderItem={item => (<TaskItem task={item} onClick={() => dispatch(actionCreators.removeTask(item.id)) } />)}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  tasks: state.tasks,
+})
+
+export default connect(mapStateToProps)(TasksList)
