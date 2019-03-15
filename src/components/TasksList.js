02a6c46727b55fd5972d9e1271557fea2cdec74e
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2019-03-15 16:56:35 -0600

    add TaskTimer to List

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -4,16 +4,16 @@ import { actionCreators } from '../actions'
 import { List, } from 'antd';
 import AddTask from './AddTask'
 import TaskItem from './TaskItem'
+import TaskTimer from './TaskTimer'
 
 class TasksList extends Component {
   render() {
-    const { tasks } = this.props
-    const { dispatch } = this.props
+    const { tasks, dispatch } = this.props
     return (
       <List
         size="large"
-        header={<AddTask />}
-        footer={<div>Footer</div>}
+        header={tasks.length > 0 ? <TaskTimer runningTask={(id) => dispatch(actionCreators.runningTask(id)) } stopTask={(id) => dispatch(actionCreators.stopTask(id)) }/> : null }
+        footer={<AddTask />}
         bordered
         dataSource={tasks}
         renderItem={item => (<TaskItem task={item} deleteTask={() => dispatch(actionCreators.removeTask(item.id)) } editTask={(item) => dispatch(actionCreators.editTask(item)) } editableTask={() => dispatch(actionCreators.editableTask(item.id)) }/>)}
