616c5c107747ad89ace30addbd09b1e459d8cd6d
Vladimir Galica <thewakosmanclub@gmail.com>
2019-10-26 22:44:31 -0600

    (TaskItem) read-only mode

---
+++ src/components/TaskItem/readOnly.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Card,
+  Typography,
+  Icon,
+  Menu,
+  Dropdown
+} from "antd";
+const { Title, Paragraph } = Typography;
+const { Item } = Menu;
+
+const TaskItem = ({task, actions}) => {
+  const menu = (
+    <Menu>
+      <Item
+        style={{ color: "#08c" }}
+        onClick={() => actions.setEditable(task.id)}
+      >
+        <Icon type="edit" />
+        <span>Edit</span>
+      </Item>
+      <Item
+        style={{ color: "red" }}
+        onClick={() => actions.remove(task.id)}
+      >
+        <Icon type="close" />
+        <span>Remove</span>
+      </Item>
+    </Menu>
+  );
+
+  const DropdownActions = (
+    <div style={{ textAlign: "end", fontSize: 25 }}>
+      <Dropdown overlay={menu}>
+        <Icon type="more" />
+      </Dropdown>
+    </div>
+  );
+
+  return (
+    <Card.Meta
+      description={
+        <Paragraph style={{ fontSize: 20 }}>{task.description}</Paragraph>
+      }
+      avatar={<Title level={2}>{task.estimated_duration}</Title>}
+      title={DropdownActions}
+    />
+  );
+};
+
+export default TaskItem;
