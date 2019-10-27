380c4ca75e38e3765e51028b3f348a91c182e1ca
Vladimir Galicia <thewakosmanclub@gmail.com>
2019-10-26 20:44:12 -0500

    change to Timeline component

--- src/components/TasksList/index.jsx
+++ src/components/TasksList/index.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { List, } from 'antd';
-import TaskItem from '../TaskItem';
+import React from "react";
+import { Timeline } from "antd";
+import TaskItem from "../TaskItem";
 
 const TasksList = ({ tasks }) => (
-  <List
-    size="large"
-    bordered
-    dataSource={tasks}
-    renderItem={item => (<TaskItem task={item} />)}
-  />
+  <Timeline>
+    {tasks.map(item => (
+      <Timeline.Item>
+        <TaskItem task={item} />
+      </Timeline.Item>
+    ))}
+  </Timeline>
 );
 
 export default TasksList;
