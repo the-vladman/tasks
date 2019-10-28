b914d44b9cae6f9474d0c4603bac7735b12ca826
Vladimir Galica <thewakosmanclub@gmail.com>
2019-10-28 00:04:13 -0600

    filter by status and put on tabPanel

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,25 +1,60 @@
+import React from "react";
+import { Tabs, Icon } from "antd";
 import { connect } from "react-redux";
 import TaskList from "../components/TasksList";
 import { actionCreators } from "../actions";
+const { TabPane } = Tabs;
+
+const TabPanel = ({ todos, completed, edit, remove, setEditable }) => {
+  const actions = { edit, remove, setEditable };
+  return (
+    <Tabs defaultActiveKey="1" style={{ textAlign: "center", fontSize: 30 }}>
+      <TabPane
+        tab={
+          <span>
+            <Icon type="bars" />
+            To do
+          </span>
+        }
+        key="1"
+      >
+        <TaskList tasks={todos} {...actions} />
+      </TabPane>
+      <TabPane
+        tab={
+          <span>
+            <Icon type="history" />
+            Completed
+          </span>
+        }
+        key="2"
+      >
+        <TaskList tasks={completed} {...actions} />
+      </TabPane>
+    </Tabs>
+  );
+};
 
 const mapState = ({ tasks }) => {
-  const tasksGroup = tasks;
+  const todos = tasks.filter(t => t.status === 0);
+  const completed = tasks.filter(t => t.status === 1);
   return {
-    tasks
+    todos,
+    completed
   };
 };
 
 const mapDispatch = dispatch => {
   return {
-    remove: (id) => dispatch(actionCreators.removeTask(id)),
-    setEditable: (id) => dispatch(actionCreators.editableTask(id)),
-    edit: (t) => dispatch(actionCreators.editTask(t)),
+    remove: id => dispatch(actionCreators.removeTask(id)),
+    setEditable: id => dispatch(actionCreators.editableTask(id)),
+    edit: t => dispatch(actionCreators.editTask(t))
   };
 };
 
 const Container = connect(
   mapState,
   mapDispatch
-)(TaskList);
+)(TabPanel);
 
 export default Container;
