26ad454fcaa01c684477c5540781e034d4ae0ff9
Vladimir Galicia <vladimir.galicia@sendengo.com>
2019-10-28 13:57:24 -0600

    Updates

---
+++ precache-manifest.ee9e7ea4a43df3af45de843d98c3ea72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d8089a231448516824e",
+    "url": "/tasks/static/js/runtime~main.b1405a7c.js"
+  },
+  {
+    "revision": "eea9740a2a5aaf0602f4",
+    "url": "/tasks/static/js/main.af92f2b6.chunk.js"
+  },
+  {
+    "revision": "aba63daa488d6b60fb18",
+    "url": "/tasks/static/js/2.5eafdc6a.chunk.js"
+  },
+  {
+    "revision": "eea9740a2a5aaf0602f4",
+    "url": "/tasks/static/css/main.1778e79b.chunk.css"
+  },
+  {
+    "revision": "aba63daa488d6b60fb18",
+    "url": "/tasks/static/css/2.383c733d.chunk.css"
+  },
+  {
+    "revision": "578919249e08031a1fbe546038a3eabc",
+    "url": "/tasks/index.html"
+  }
+];
