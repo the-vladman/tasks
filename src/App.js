b489481edb479b99507c95a2b7cc5b78ceefa396
Vladimir Galica <thewakosmanclub@gmail.com>
2019-10-27 22:04:18 -0600

    add containers for component aactions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
-import {  Row, Col, Layout, } from 'antd';
-import { Add, List, Timer } from './containers';
-const { Header, Content } = Layout
+import React, { Component } from "react";
+import "./App.css";
+import { Row, Col, Layout } from "antd";
+import { Add, List, Timer } from "./containers";
+const { Header, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          <Header/>
-          <Content style={{ padding: 24, margin: 0, minHeight: '100vh', }}>
-            <Row type="flex" justify="center">
+          <Header />
+          <Content style={{ padding: 24, margin: 0, minHeight: "100vh" }}>
+            <Row type="flex" justify="center" style={{ marginTop: 24 }}>
               <Col span={12}>
-                <Add />
+                <Timer />
               </Col>
             </Row>
             <Row type="flex" justify="center" style={{ marginTop: 24 }}>
               <Col span={12}>
                 <List />
+              </Col>
+            </Row>
+            <Row type="flex" justify="center">
+              <Col span={12}>
+                <Add />
               </Col>
             </Row>
           </Content>
